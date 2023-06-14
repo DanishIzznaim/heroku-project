39885a6870febd3de679303ea066b790ac9d08ca
harisjml <2021849886@student.uitm.edu.my>
2023-06-15 04:30:21 +0800

    CREATE READ

--- src/main/java/com/heroku/java/customerController.java
+++ src/main/java/com/heroku/java/customerController.java
@@ -1,15 +1,18 @@
 package com.heroku.java;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.web.bind.annotation.PostMapping;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import jakarta.servlet.http.HttpSession;
 import javax.sql.DataSource;
 import java.sql.*;
+// import java.text.SimpleDateFormat;
 
 @Controller
 public class customerController {
@@ -65,7 +68,7 @@ public class customerController {
 
   }
   @PostMapping("/login") 
-    public String HomePage(HttpSession session, @ModelAttribute("login") Customer customer) { 
+    public String HomePage(HttpSession session, @ModelAttribute("login") Customer customer, Model model) { 
         try (
             Connection connection = dataSource.getConnection()) { 
             final var statement = connection.createStatement(); 
@@ -96,5 +99,42 @@ public class customerController {
  
     }
 
+        @GetMapping("/profilecust")
+        public String viewProfile(HttpSession session, Customer customer, Model model) {
+        String username = (String) session.getAttribute("username");
+    
+    
+        if (username != null) { 
+        try (Connection connection = dataSource.getConnection()) {
+            final var statement = connection.prepareStatement("SELECT fullname, address, phonenum, icnumber, licensecard, username, password FROM customer WHERE username = ? ");
+            statement.setString(1, username);
+            final var resultSet = statement.executeQuery();
+
+            while(resultSet.next()){
+                String fullname = resultSet.getString("fullname");
+                String address = resultSet.getString("address");
+                String phonenum = resultSet.getString("phonenum");
+                String icnumber = resultSet.getString("icnumber");
+                Date licensecard = resultSet.getDate("licensecard");
+                String password = resultSet.getString("password");
+
+                System.out.println("fullname from db: " + fullname);
+                Customer profilecust = new Customer(fullname, address, phonenum, icnumber, licensecard, username, password);
+                model.addAttribute("profilecust", profilecust);
+                System.out.println("Session profileCust : " + model.getAttribute("profilecust"));
+                // Return the view name for displaying customer details
+            }
+                
+                return "profilecust";
+            
+        } catch (SQLException e) {
+            // Handle the exception (e.g., log the error, display an error page)
+            e.printStackTrace();
+        }
+        }
+    
+        // Customer not found or username is null, handle accordingly (e.g., redirect to an error page)
+        return "error";
+        }
 
 }
