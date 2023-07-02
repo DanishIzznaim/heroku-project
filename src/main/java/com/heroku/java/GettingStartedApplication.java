8267211d66c3bc0bb0411b27c6cd8695062d21e2
DanishIzznaim <2021453816@student.uitm.edu.my>
2023-07-02 23:02:30 +0800

    Addcarfrm.html Latest

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -26,10 +26,10 @@ public class GettingStartedApplication {
         this.dataSource = dataSource;
     }
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
+    // @GetMapping("/index")
+    // public String index() {
+    //     return "index";
+    // }
 
     @GetMapping("/homepage")
     public String homepage() {
@@ -166,11 +166,6 @@ public class GettingStartedApplication {
     public String addcarform() {
         return "admin/addcarform";
     }
-
-    // @GetMapping("/profilecust")
-    // public String profilecust() {
-    //     return "profilecust";
-    // }
     
     @GetMapping("/logout")
     public String logout(HttpSession session) {
@@ -202,6 +197,16 @@ public class GettingStartedApplication {
     public String addcarmpv() {
         return "admin/addcarmpv";
     }
+
+    @GetMapping("/approval")
+    public String approval() {
+        return "admin/approval";
+    }
+
+    // @GetMapping("/profilecust")
+    // public String profilecust() {
+    //     return "profilecust";
+    // }
 
     @GetMapping("/database")
     String database(Map<String, Object> model) {
